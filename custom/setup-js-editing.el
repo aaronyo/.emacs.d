1737d6a2e058a7ef3d00fa70b0a6c5059b524cb2
Aaron Boyd <aaron.t.boyd@gmail.com>
2017-02-10 17:05:17 -0800

    merge set-eslint-exec; use local prettier install

--- custom/setup-js-editing.el
+++ custom/setup-js-editing.el
@@ -13,8 +13,34 @@
  fci-rule-color "color-236")
 (add-hook 'js2-mode-hook 'flycheck-mode)
 (add-hook 'js2-mode-hook 'fci-mode)
+
 ;; Set eslint executable based on buffer
-(load-library "set-eslint-executable")
+(defun my/use-eslint-from-node-modules ()
+  (let* ((root (locate-dominating-file
+                (or (buffer-file-name) default-directory)
+                ".eslintrc"))
+         (eslint (and root
+                      (expand-file-name "node_modules/eslint/bin/eslint.js"
+                                        root))))
+    (when (and eslint (file-executable-p eslint))
+      (setq-local flycheck-javascript-eslint-executable eslint))))
+
+(add-hook 'flycheck-mode-hook #'my/use-eslint-from-node-modules)
+
+;; Set prettier executable based on buffer
+(defun my/use-prettier-from-node-modules ()
+  (let* ((root (locate-dominating-file
+                (or (buffer-file-name) default-directory)
+                ".prettierrc"))
+         (prettier (and root
+                      (expand-file-name "node_modules/prettier/bin/prettier.js"
+                                        root))))
+    (when (and prettier (file-executable-p prettier))
+      (setq-local prettier-command prettier))))
+
+(add-hook 'js2-mode-hook #'my/use-prettier-from-node-modules)
+
+
 
 (require 'prettier-js)
 (add-hook 'js-mode-hook
