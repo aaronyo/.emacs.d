8d547262763c82fdaaa54c6260ed99034d8ee12d
Aaron Boyd <aaron.t.boyd@gmail.com>
2017-02-24 07:23:44 -0800

    change location of eslint and prettier executables

--- custom/setup-js-editing.el
+++ custom/setup-js-editing.el
@@ -20,7 +20,7 @@
                 (or (buffer-file-name) default-directory)
                 ".eslintrc"))
          (eslint (and root
-                      (expand-file-name "node_modules/eslint/bin/eslint.js"
+                      (expand-file-name "bin/eslint"
                                         root))))
     (when (and eslint (file-executable-p eslint))
       (setq-local flycheck-javascript-eslint-executable eslint))))
@@ -31,9 +31,9 @@
 (defun my/use-prettier-from-node-modules ()
   (let* ((root (locate-dominating-file
                 (or (buffer-file-name) default-directory)
-                ".prettierrc"))
+                ".eslintrc"))
          (prettier (and root
-                      (expand-file-name "node_modules/prettier/bin/prettier.js"
+                      (expand-file-name "bin/prettier"
                                         root))))
     (when (and prettier (file-executable-p prettier))
       (setq-local prettier-command prettier))))
