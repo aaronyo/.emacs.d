918b69e2f74882abaa4c38abf522359378aa547c
Aaron Boyd <aaron.t.boyd@gmail.com>
2019-12-06 10:37:31 -0800

    use helper for finding dominating executables

--- custom/setup-js-editing.el
+++ custom/setup-js-editing.el
@@ -13,54 +13,42 @@
 
 ;; (add-hook 'rjsx-mode-hook 'fci-mode)
 
-;; Set eslint executable based on buffer
-(defun my/use-eslint-from-node-modules ()
+
+(defun my/find-dominating (rel-path)
   (let* ((root (locate-dominating-file
                 (or (buffer-file-name) default-directory)
-                "bin/eslint"))
-         (eslint (and root
-                      (expand-file-name "bin/eslint"
+                rel-path))
+         (full-path (and root
+                      (expand-file-name rel-path
                                         root))))
-    (when (and eslint (file-executable-p eslint))
-      (setq-local flycheck-javascript-eslint-executable eslint))))
+    full-path))
 
-(add-hook 'flycheck-mode-hook #'my/use-eslint-from-node-modules)
+;; Set eslint executable based on buffer
+(defun my/use-dominating-eslint ()
+  (setq-local flycheck-javascript-eslint-executable (my/find-dominating "bin/eslint")))
+
+(add-hook 'flycheck-mode-hook #'my/use-dominating-eslint)
 
 ;; Set prettier executable based on buffer
-(defun my/use-prettier-from-node-modules ()
-  (let* ((root (locate-dominating-file
-                (or (buffer-file-name) default-directory)
-                "bin/prettier"))
-         (prettier (and root
-                      (expand-file-name "bin/prettier"
-                                        root))))
-    (when (and prettier (file-executable-p prettier))
-      (setq-local prettier-js-command prettier))))
+(defun my/use-dominating-prettier ()
+  (setq-local prettier-js-command (my/find-dominating "bin/prettier")))
 
-(add-hook 'js2-mode-hook #'my/use-prettier-from-node-modules)
-(add-hook 'rjsx-mode-hook #'my/use-prettier-from-node-modules)
+(add-hook 'js2-mode-hook #'my/use-dominating-prettier)
+(add-hook 'rjsx-mode-hook #'my/use-dominating-prettier)
 
 ;;
 ;; Tide and typescript for esling
 ;;
 
 ;; Set prettier executable based on buffer
-(defun my/use-tsc-from-node-modules ()
-  (let* ((root (locate-dominating-file
-                (or (buffer-file-name) default-directory)
-                "bin/tsc"))
-         (tsserver (and root
-                      (expand-file-name "bin/tsserver"
-                                        root))))
-    (when (and tsserver (file-executable-p tsserver))
-      (setq-local tide-tsserver-executable tsserver)
-      )))
+(defun my/use-dominating-tsserver ()
+  (setq-local tide-tsserver-executable (my/find-dominating "bin/tsserver")))
 
 (with-eval-after-load 'flycheck
   (flycheck-add-mode 'javascript-eslint 'typescript-mode))
 
 (defun setup-tide-mode ()
-  (my/use-tsc-from-node-modules)
+  (my/use-dominating-tsserver)
   (interactive)
   (tide-setup)
   (flycheck-mode +1)
@@ -90,8 +78,8 @@
 ;; (add-hook 'before-save-hook 'tide-format-before-save)
 
 (add-hook 'typescript-mode-hook #'setup-tide-mode)
-(add-hook 'typescript-mode-hook #'my/use-prettier-from-node-modules)
-(add-hook 'typescript-mode-hook #'my/use-eslint-from-node-modules)
+(add-hook 'typescript-mode-hook #'my/use-dominating-prettier)
+(add-hook 'typescript-mode-hook #'my/use-dominating-eslint)
 (add-hook 'typescript-mode-hook 'prettier-js-mode)
 (add-hook 'typescript-mode-hook 'fci-mode)
 
