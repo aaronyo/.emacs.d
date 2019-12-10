1492cd1231928089d28c0ae88c39ec9ac741f26e
Aaron Boyd <aaron.t.boyd@gmail.com>
2019-12-10 15:13:55 -0800

    fix type

--- init.el
+++ init.el
@@ -153,7 +153,7 @@ your recently and most frequently used commands.")
 
   (defun company-turn-off-fci (&rest ignore)
     (when (boundp 'fci-mode)
-      (setq-locall company-fci-mode-on-p fci-mode)
+      (setq-local company-fci-mode-on-p fci-mode)
       (when fci-mode (fci-mode -1))))
 
   (defun company-maybe-turn-on-fci (&rest ignore)
