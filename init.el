c13769ecd4682e8e5002d557c6e882b00d464178
Aaron Boyd <aaron.t.boyd@gmail.com>
2019-12-12 22:31:41 -0800

    fixup counsel and ivy-rich configs

--- init.el
+++ init.el
@@ -22,10 +22,6 @@
 
 (defvar-local window-background "#181818")
 
-(use-package counsel
-  :after ivy
-  :config (counsel-mode))
-
 (use-package ivy
   :defer 0.1
   :diminish
@@ -36,18 +32,17 @@
   (ivy-use-virtual-buffers t)
   :config (ivy-mode))
 
-(require 'ivy)
-(use-package ivy-rich
+(use-package counsel
   :after ivy
-  :defer .1
-  :custom
-  (ivy-virtual-abbreviate 'full
-                          ivy-rich-switch-buffer-align-virtual-buffer t
-                          ivy-rich-path-style 'abbrev)
-  :config
-  (eval-after-load 'ivy
-    (ivy-set-display-transformer 'ivy-switch-buffer
-                                 'ivy-rich-switch-buffer-transformer)))
+  :config (counsel-mode))
+
+(use-package ivy-rich
+  :after (ivy)
+  :init
+  (setq ivy-rich-path-style 'abbrev
+        ivy-virtual-abbreviate 'full)
+  :config (ivy-rich-mode 1))
+
 
 (use-package swiper
   :after ivy
