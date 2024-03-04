cd24df4df1103cdbb23a60209542fe9e1978c713
Aaron Boyd <aaron.t.boyd@gmail.com>
2024-03-04 11:21:32 -0800

    use eglot for python

--- lisp/my/setup-python-editing.el
+++ lisp/my/setup-python-editing.el
@@ -4,28 +4,35 @@
 
 ;;; Code:
 
-(use-package jedi)
 
-(use-package elpy
-  :ensure t
-  :init
-  (add-hook 'elpy-mode-hook 'jedi:setup)
-  (add-hook 'python-mode-hook (lambda () (auto-complete-mode -1)))
-  (setq ac-modes (delq 'python-mode ac-modes))
-  (elpy-enable)
-  (setq elpy-rpc-virtualenv-path 'current)
-  (add-to-list 'process-coding-system-alist '("python" . (utf-8 . utf-8)))
-  )
+(add-to-list 'auto-mode-alist '("\\.py\\'" . python-mode))
 
-;; Enable Flycheck
-(when (require 'flycheck nil t)
-  (setq elpy-modules (delq 'elpy-module-flymake elpy-modules))
-  (add-hook 'elpy-mode-hook 'flycheck-mode))
+(use-package python-black
+  :demand t
+  :after python
+  :hook (python-mode . python-black-on-save-mode))
 
-(use-package blacken
-  :init
-  (setq blacken-line-length 79))
+;;(use-package jedi)
 
-(use-package py-isort)
+;; (use-package elpy
+;;   :ensure t
+;;   :init
+;; ;;  (add-hook 'elpy-mode-hook 'jedi:setup)
+;;   (add-hook 'python-mode-hook (lambda () (auto-complete-mode -1)))
+;;   (elpy-enable)
+;; ;;  (setq elpy-rpc-virtualenv-path 'current)
+;;   (add-to-list 'process-coding-system-alist '("python3.12" . (utf-8 . utf-8)))
+;;   )
+
+;; ;; Enable Flycheck
+;; (when (require 'flycheck nil t)
+;;   (setq elpy-modules (delq 'elpy-module-flymake elpy-modules))
+;;   (add-hook 'elpy-mode-hook 'flycheck-mode))
+
+;; (use-package blacken
+;;   :init
+;;   (setq blacken-line-length 79))
+
+;; (use-package py-isort)
 
 ;;; setup-python-editing ends here
